97f47a89051bc0c0662e485b4e1f55b460d557cb
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2024-10-09 22:55:51 +0800

    feat: add brands logo, position items using grid, incl sm,md,lg view

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -1,5 +1,10 @@
-import React from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import GoogleLogo from "../assets/images/G.png";
+import NetflixLogo from "../assets/images/net.png";
+import AirbnbLogo from "../assets/images/air.png";
+import AmazonLogo from "../assets/images/amazon.png";
+import FacebookLogo from "../assets/images/fb.png";
+import GrabLogo from "../assets/images/grab.png";
 const Brands = () => {
   return (
     <section>
@@ -11,6 +16,46 @@ const Brands = () => {
             Worldwide
           </p>
           <FaChevronRight className="text-primary text-2xl" />
+        </div>
+        <div className="grid grid-cols-1 gap-2 md:grid-cols-3 lg:grid-cols-6 py-14 space-y-4 lg:space-y-0">
+          <div className="flex items-center justify-center">
+            <img
+              src={GoogleLogo}
+              alt="google-logo"
+              className="h-auto w-[151px]"
+            />
+          </div>
+          <div className="flex items-center justify-center">
+            <img
+              src={NetflixLogo}
+              alt="netflix-logo"
+              className="h-auto w-[151px]"
+            />
+          </div>
+          <div className="flex items-center justify-center">
+            <img
+              src={AirbnbLogo}
+              alt="airbnb-logo"
+              className="h-auto w-[151px]"
+            />
+          </div>
+          <div className="flex items-center justify-center">
+            <img
+              src={AmazonLogo}
+              alt="amazon-logo"
+              className="h-auto w-[151px]"
+            />
+          </div>
+          <div className="flex items-center justify-center">
+            <img
+              src={FacebookLogo}
+              alt="facebook-logo"
+              className="h-auto w-[151px]"
+            />
+          </div>
+          <div className="flex items-center justify-center">
+            <img src={GrabLogo} alt="grab-logo" className="h-auto w-[151px]" />
+          </div>
         </div>
       </div>
     </section>
