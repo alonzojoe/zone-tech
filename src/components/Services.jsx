f4aa8c226db8167794367e63daab020576ab7f86
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2024-10-10 22:59:42 +0800

    feat: finalized ui ux services section add absolute image and card titled icon

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,3 +1,5 @@
+import { FaShopify } from "react-icons/fa";
+import { BsRocketTakeoffFill } from "react-icons/bs";
 import BoxLeft from "../assets/images/box-left.png";
 import CircRight from "../assets/images/circ-right.png";
 
@@ -15,34 +17,50 @@ const Services = () => {
             text ever since the 1500s
           </p>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 py-14">
-          <div className="flex flex-col bg-red items-center opcaity-100 justify-center relative p-4 px-7 space-y-8 rounded-xl border-[1px] border-green-500 z-10">
-            <h3 className="text-2xl font-semibold">Shopify Development</h3>
-            <p className="text-center text-lighter">
-              “ Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry&apos;s standard dummy
-              text ever since the 1500s. Lorem. “
-            </p>
-            <div>
-              <button className="primary-btn">Read more</button>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 py-24 space-y-8 md:space-y-0">
+          <div className="relative z-0">
+            <div className="flex flex-col bg-white items-center justify-center p-4 px-7 space-y-8 rounded-xl relative z-20 border-[1px] border-transparent hover:border-barrier">
+              <div className="h-[100px] w-[100px] bg-primary rounded-full flex items-center justify-center absolute z-30 -top-14">
+                <FaShopify className="text-white text-5xl" />
+              </div>
+              <h3 className="text-2xl font-semibold pt-5">
+                Shopify Development
+              </h3>
+              <p className="text-center text-lighter">
+                “ Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the industry&apos;s
+                standard dummy text ever since the 1500s. Lorem. “
+              </p>
+              <div className="pb-4">
+                <button className="secondary-btn">Read more</button>
+              </div>
             </div>
             <img
               src={BoxLeft}
               alt="box-left"
-              className="absolute -bottom-5 -left-10 -z-10"
+              className="absolute -bottom-5 -left-10 w-[198px] h-auto"
             />
           </div>
-          <div className="flex flex-col items-center justify-center relative p-4 px-7 space-y-8  rounded-xl border-[1px] border-blue-500">
-            <h3 className="text-2xl font-semibold">Digital Marketing</h3>
-            <p className="text-center text-lighter">
-              “ Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry&apos;s standard dummy
-              text ever since the 1500s. Lorem. “
-            </p>
-            <div>
-              <button className="primary-btn">Read more</button>
+          <div className="relative z-0">
+            <div className="flex flex-col bg-white items-center justify-center relative p-4 px-7 space-y-8 rounded-xl z-20 border-[1px] border-transparent hover:border-barrier">
+              <div className="h-[100px] w-[100px] bg-secondary rounded-full flex items-center justify-center absolute z-30 -top-14">
+                <BsRocketTakeoffFill className="text-white text-5xl" />
+              </div>
+              <h3 className="text-2xl font-semibold pt-5">Digital Marketing</h3>
+              <p className="text-center text-lighter">
+                “ Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the industry&apos;s
+                standard dummy text ever since the 1500s. Lorem. “
+              </p>
+              <div className="pb-4">
+                <button className="secondary-btn">Read more</button>
+              </div>
             </div>
-            {/* <img src={CircRight} alt="circle-right" /> */}
+            <img
+              src={CircRight}
+              alt="circle-right"
+              className="absolute -top-10 -right-20 w-[156.44] h-auto"
+            />
           </div>
         </div>
       </div>
